b045e43be575f8597b9593a3520e974fdeb80387
sugimx <sugix.info@gmail.com>
2025-10-21 11:31:14 +0530

    Update Address.js

--- src/models/Address.js
+++ src/models/Address.js
@@ -18,59 +18,36 @@ const addressSchema = new mongoose.Schema({
   fullName: {
     type: String,
     required: [true, 'Please add full name'],
-    trim: true,
   },
   phone: {
     type: String,
     required: [true, 'Please add phone number'],
-    trim: true,
   },
-  address: {
+  addressLine1: {
     type: String,
     required: [true, 'Please add address line 1'],
-    trim: true,
   },
+  addressLine2: String,
   city: {
     type: String,
     required: [true, 'Please add city'],
-    trim: true,
   },
   state: {
     type: String,
     required: [true, 'Please add state'],
-    trim: true,
   },
   postalCode: {
     type: String,
     required: [true, 'Please add postal code'],
-    trim: true,
   },
   country: {
     type: String,
-    required: [true, 'Please add country'],
-    trim: true,
     default: 'India',
   },
-  landmark: {
-    type: String,
-    trim: true,
-  }
+  landmark: String,
+  instructions: String,
 }, {
   timestamps: true,
-});
-
-// Index for faster queries
-addressSchema.index({ user: 1 });
-
-// Middleware to ensure only one default address per user
-addressSchema.pre('save', async function(next) {
-  if (this.isDefault) {
-    await this.constructor.updateMany(
-      { user: this.user, _id: { $ne: this._id } },
-      { isDefault: false }
-    );
-  }
-  next();
 });
 
 module.exports = mongoose.model('Address', addressSchema);
