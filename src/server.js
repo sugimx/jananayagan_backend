6a1900a9cc85e0014b272993f11d88f5d2daabaf
Sri Sitharth <sitharth@sevmobility.com>
2026-05-07 15:49:50 +0530

    Revert test change

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ connectDB();
 
 // Routes
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to Giveway Backend API - v2' });
+  res.json({ message: 'Welcome to Giveway Backend API' });
 });
 
 // Import routes
